cdce15b9977fd91d13c63bc7a7c4639c2a3c189e
007 <dr007cc@gmail.com>
2024-09-26 21:09:52 +0330

    feat(web): docs and config

---
+++ web/src/config.rs
@@ -0,0 +1,26 @@
+use std::sync::OnceLock;
+
+#[derive(Debug)]
+/// Main Config
+pub struct Config {}
+
+macro_rules! evar {
+    ($name:literal) => {
+        std::env::var($name).expect(concat!($name, " was not in .env"))
+    };
+}
+pub(crate) use evar;
+
+impl Config {
+    pub const RECORD_DIR: &'static str = "record";
+    pub const CODE_ABC: &'static [u8] = b"0123456789";
+    pub const TOKEN_ABC: &'static [u8] =
+        b"!@#$%^&*_+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_+";
+    pub const SLUG_ABC: &'static [u8] =
+        b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+}
+
+pub fn config() -> &'static Config {
+    static STATE: OnceLock<Config> = OnceLock::new();
+    STATE.get_or_init(|| Config {})
+}
