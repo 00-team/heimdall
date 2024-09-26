8dd01e78f3688b2c9206c401d8b4b7de81278ec4
007 <dr007cc@gmail.com>
2024-09-26 21:29:41 +0330

    feat(web): init api

---
+++ web/src/api/user.rs
@@ -0,0 +1,158 @@
+use crate::api::verification;
+use crate::config::Config;
+use crate::docs::UpdatePaths;
+use crate::models::user::User;
+use crate::models::{AppErr, Response};
+use crate::utils::{self, CutOff};
+use crate::AppState;
+
+use actix_web::cookie::time::Duration;
+use actix_web::cookie::{Cookie, SameSite};
+use actix_web::web::{Data, Json};
+use actix_web::{get, patch, post, HttpResponse, Scope};
+use serde::Deserialize;
+use utoipa::{OpenApi, ToSchema};
+
+#[derive(OpenApi)]
+#[openapi(
+    tags((name = "api::user")),
+    paths(login, logout, get, update),
+    components(schemas(User, LoginBody, UserUpdateBody)),
+    servers((url = "/user")),
+    modifiers(&UpdatePaths)
+)]
+pub struct ApiDoc;
+
+#[derive(Debug, Deserialize, ToSchema)]
+struct LoginBody {
+    phone: String,
+    code: String,
+}
+
+#[utoipa::path(
+    post,
+    request_body = LoginBody,
+    responses((status = 200, body = User))
+)]
+/// Login
+#[post("/login/")]
+async fn login(body: Json<LoginBody>, state: Data<AppState>) -> Result<HttpResponse, AppErr> {
+    verification::verify(&body.phone, &body.code, verification::Action::Login).await?;
+
+    let token = utils::get_random_string(Config::TOKEN_ABC, 69);
+
+    let result = sqlx::query_as! {
+        User,
+        "select * from users where phone = ?",
+        body.phone
+    }
+    .fetch_one(&state.sql)
+    .await;
+
+    let user: User = match result {
+        Ok(mut user) => {
+            user.token = Some(token.clone());
+
+            let _ = sqlx::query_as! {
+                User,
+                "update users set token = ? where id = ?",
+                token, user.id
+            }
+            .execute(&state.sql)
+            .await;
+
+            user
+        }
+        Err(_) => {
+            let result = sqlx::query_as! {
+                User,
+                "insert into users (phone, token) values(?, ?)",
+                body.phone, token
+            }
+            .execute(&state.sql)
+            .await;
+
+            User {
+                id: result.unwrap().last_insert_rowid(),
+                phone: body.phone.clone(),
+                token: Some(token.clone()),
+                ..Default::default()
+            }
+        }
+    };
+
+    let cook = Cookie::build("authorization", format!("user {}:{token}", user.id))
+        .path("/")
+        .secure(true)
+        .same_site(SameSite::Lax)
+        .http_only(true)
+        .max_age(Duration::weeks(12))
+        .finish();
+
+    Ok(HttpResponse::Ok().cookie(cook).json(user))
+}
+
+#[utoipa::path(post, responses((status = 200)))]
+#[post("/logout/")]
+/// Logout
+async fn logout(user: User, state: Data<AppState>) -> HttpResponse {
+    let _ = sqlx::query! {
+        "update users set token = null where id = ?",
+        user.id
+    }
+    .execute(&state.sql)
+    .await;
+
+    let cook = Cookie::build("authorization", "")
+        .path("/")
+        .secure(true)
+        .same_site(SameSite::Lax)
+        .http_only(true)
+        .max_age(Duration::seconds(1))
+        .finish();
+
+    HttpResponse::Ok().cookie(cook).finish()
+}
+
+#[utoipa::path(get, responses((status = 200, body = User)))]
+#[get("/")]
+/// Get
+async fn get(user: User) -> Json<User> {
+    Json(user)
+}
+
+#[derive(Deserialize, ToSchema)]
+struct UserUpdateBody {
+    name: String,
+}
+
+#[utoipa::path(
+    patch,
+    request_body = UserUpdateBody,
+    responses((status = 200, body = User))
+)]
+/// Update
+#[patch("/")]
+async fn update(user: User, body: Json<UserUpdateBody>, state: Data<AppState>) -> Response<User> {
+    let mut user = user;
+    user.name = body.name.clone();
+    user.name.cut_off(255);
+
+    sqlx::query_as! {
+        User,
+        "update users set name = ? where id = ?",
+        user.name, user.id
+    }
+    .execute(&state.sql)
+    .await?;
+
+    Ok(Json(user))
+}
+
+pub fn router() -> Scope {
+    Scope::new("/user")
+        .service(login)
+        .service(logout)
+        .service(get)
+        .service(update)
+}
---
+++ web/src/api/verification.rs
@@ -0,0 +1,128 @@
+use actix_web::{post, web::Json};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, sync::OnceLock};
+use tokio::sync::Mutex;
+use utoipa::{OpenApi, ToSchema};
+
+use crate::{
+    config::Config,
+    models::{self, AppErr, AppErrBadRequest},
+    utils,
+};
+use models::Response;
+
+#[derive(PartialEq, Debug, Clone, Deserialize, Serialize, ToSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum Action {
+    Login,
+    Delete,
+}
+
+struct VerifyData {
+    action: Action,
+    code: String,
+    expires: i64,
+    tries: u8,
+}
+
+type VerifyState = Mutex<HashMap<String, VerifyData>>;
+
+fn verify_state() -> &'static VerifyState {
+    static STATE: OnceLock<VerifyState> = OnceLock::new();
+    STATE.get_or_init(|| VerifyState::new(HashMap::new()))
+}
+
+#[derive(OpenApi)]
+#[openapi(
+    paths(verification),
+    components(schemas(VerificationData, VerificationResponse, Action))
+)]
+pub struct ApiDoc;
+
+#[derive(ToSchema, Deserialize, Debug)]
+struct VerificationData {
+    phone: String,
+    action: Action,
+}
+
+#[derive(ToSchema, Serialize, Debug)]
+struct VerificationResponse {
+    expires: i64,
+    action: Action,
+}
+
+#[utoipa::path(
+    post,
+    request_body = VerificationData,
+    responses(
+        (status = 200, body = VerificationResponse)
+    )
+)]
+/// Verification
+#[post("/verification/")]
+async fn verification(body: Json<VerificationData>) -> Response<VerificationResponse> {
+    let now = utils::now();
+    utils::phone_validator(&body.phone)?;
+
+    let mut vdb = verify_state().lock().await;
+    let result = vdb.get(&body.phone);
+
+    if let Some(v) = result {
+        let t = v.expires - now;
+        if t > 0 {
+            return Ok(Json(VerificationResponse {
+                expires: t,
+                action: v.action.clone(),
+            }));
+        }
+    }
+
+    vdb.retain(|_, v| v.expires - now > 0);
+
+    let code = utils::get_random_string(Config::CODE_ABC, 5);
+    log::info!("code: {code}");
+
+    vdb.insert(
+        body.phone.clone(),
+        VerifyData {
+            action: body.action.clone(),
+            code,
+            expires: now + 180,
+            tries: 0,
+        },
+    );
+
+    Ok(Json(VerificationResponse {
+        expires: 180,
+        action: body.action.to_owned(),
+    }))
+}
+
+pub async fn verify(phone: &str, code: &str, action: Action) -> Result<(), AppErr> {
+    let now = utils::now();
+
+    let mut vdb = verify_state().lock().await;
+    vdb.retain(|_, v| v.expires - now > 0);
+
+    let v = vdb
+        .get_mut(phone)
+        .ok_or(AppErrBadRequest("bad verification"))?;
+
+    v.tries += 1;
+
+    if v.action != action {
+        return Err(AppErrBadRequest("invalid action"));
+    }
+
+    if v.code != code {
+        if v.tries > 2 {
+            return Err(AppErrBadRequest("too many tries"));
+        }
+
+        return Err(AppErrBadRequest("invalid code"));
+    }
+
+    vdb.remove(phone);
+
+    Ok(())
+}
