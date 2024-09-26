f8262d4e4224d844b2d67e4ef670fb7fd4044298
007 <dr007cc@gmail.com>
2024-09-26 21:42:13 +0330

    fix(web): set user phone to be unique

--- web/migrations/000_init.sql
+++ web/migrations/000_init.sql
@@ -1,6 +1,6 @@
 create table if not exists users (
     id integer primary key not null,
-    phone text not null,
+    phone text not null unique,
     name text not null default "",
     token text,
     admin boolean not null default false
