b8881da128552009bfbd18fe3a14d9d5f3a15b64
007 <dr007cc@gmail.com>
2024-09-26 21:49:52 +0330

    feat(web): init projects table

--- web/migrations/000_init.sql
+++ web/migrations/000_init.sql
@@ -5,3 +5,10 @@ create table if not exists users (
     token text,
     admin boolean not null default false
 );
+
+create table if not exists projects (
+    slug text primary key not null,
+    latest_request integer not null default 0, 
+    total_requests integer not null default 0,
+    token text
+);
