f7c81023126b247bec475ca09ba3d00c3363f877
007 <dr007cc@gmail.com>
2024-12-13 22:10:16 +0330

    fix: update

--- dog/src/main.rs
+++ dog/src/main.rs
@@ -7,15 +7,35 @@ use std::{
 };
 
 use serde::Serialize;
-use serde_tuple::Deserialize_tuple;
 
 const API_DUMP: &str = "https://heimdall.00-team.org/api/sites/dump/";
 const API_PING: &str = "https://heimdall.00-team.org/api/sites/ping/";
 
-#[derive(Debug, Deserialize_tuple)]
+#[derive(Debug, Default)]
 struct Message {
     status: u16,
     upstream_response_time: f64,
+}
+
+impl From<&[u8]> for Message {
+    fn from(value: &[u8]) -> Self {
+        let mut msg = Self::default();
+        if value.len() < 3 {
+            return msg;
+        }
+
+        let mut it = value[1..value.len() - 1].splitn(2, |v| *v == b',');
+
+        if let Some(v) = it.next().and_then(|v| std::str::from_utf8(v).ok()) {
+            msg.status = v.parse().unwrap_or_default();
+        }
+
+        if let Some(v) = it.next().and_then(|v| std::str::from_utf8(v).ok()) {
+            msg.upstream_response_time = v.parse().unwrap_or_default();
+        }
+
+        msg
+    }
 }
 
 #[derive(Serialize, Default, Debug)]
@@ -59,7 +79,7 @@ fn main() -> std::io::Result<()> {
                 continue;
             };
 
-            if output.stdout == b"active" {
+            if output.stdout == b"active\n" {
                 client.post(API_PING).body("").send().unwrap();
             }
 
@@ -100,47 +120,40 @@ fn main() -> std::io::Result<()> {
                 continue;
             }
         };
-        match serde_json::from_slice::<Message>(&buf[24..size]) {
-            Ok(msg) => {
-                let time = (msg.upstream_response_time * 1000.0) as u64;
-                dump.total += 1;
-                dump.total_time += time;
-                if dump.max_time < time {
-                    dump.max_time = time;
-                }
+        let msg = Message::from(&buf[24..size]);
+        let time = (msg.upstream_response_time * 1000.0) as u64;
+        dump.total += 1;
+        dump.total_time += time;
+        if dump.max_time < time {
+            dump.max_time = time;
+        }
 
-                if dump.min_time > time || dump.min_time == 0 {
-                    dump.min_time = time;
-                }
+        if dump.min_time > time || dump.min_time == 0 {
+            dump.min_time = time;
+        }
 
-                let sk = msg.status.to_string();
-                if let Some(status) = dump.status.get_mut(&sk) {
-                    status.count += 1;
-                    status.total_time += time;
+        let sk = msg.status.to_string();
+        if let Some(status) = dump.status.get_mut(&sk) {
+            status.count += 1;
+            status.total_time += time;
 
-                    if status.min_time > time || status.min_time == 0 {
-                        status.min_time = time;
-                    }
-                    if status.max_time < time {
-                        status.max_time = time;
-                    }
-                } else {
-                    dump.status.insert(
-                        sk,
-                        Status {
-                            code: msg.status,
-                            total_time: time,
-                            min_time: time,
-                            max_time: time,
-                            count: 1,
-                        },
-                    );
-                }
+            if status.min_time > time || status.min_time == 0 {
+                status.min_time = time;
             }
-            Err(e) => println!(
-                "err: {e}\n[{size}]: {}",
-                String::from_utf8_lossy(&buf[..size])
-            ),
+            if status.max_time < time {
+                status.max_time = time;
+            }
+        } else {
+            dump.status.insert(
+                sk,
+                Status {
+                    code: msg.status,
+                    total_time: time,
+                    min_time: time,
+                    max_time: time,
+                    count: 1,
+                },
+            );
         }
     }
 }
