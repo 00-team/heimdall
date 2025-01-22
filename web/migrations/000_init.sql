0ccf7c587c495ddaa5451fd856c26c821e04811e
007 <dr007cc@gmail.com>
2025-01-22 20:03:24 +0330

    fix: merge migs

--- web/migrations/000_init.sql
+++ web/migrations/000_init.sql
@@ -30,3 +30,15 @@ create table if not exists sites_messages (
     text text not null,
     tag text not null
 );
+
+create table if not exists deploys (
+    id integer primary key not null,
+    repo text not null, -- simurgh
+    actor text not null, -- github
+    sender text, -- 007, sadra, ...
+    begin integer not null default 0,
+    finish integer not null default 0,
+    status integer not null default 0, -- pending, running, failed, success
+    stdout text,
+    stderr text
+);
